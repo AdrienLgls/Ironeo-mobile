b4040683c327fee0f60b26c42b04d1cf3c0a6ec8
Adrien <adrien@ironeo.com>
2026-03-21 12:49:46 +0000

    feat(chat): add haptic feedback on message send

--- components/social/GroupChatTab.tsx
+++ components/social/GroupChatTab.tsx
@@ -14,6 +14,7 @@ import {
 import { useToast } from '../../context/ToastContext';
 import api from '../../services/api';
 import { formatChatTimestamp } from '../../utils/formatters';
+import { hapticImpact } from '../../utils/haptics';
 import {
   deleteMessage,
   getMessages,
@@ -203,6 +204,7 @@ export default function GroupChatTab({ groupId }: Props) {
     setSending(true);
     try {
       const sent = await sendMessage(groupId, text, replyTo?._id);
+      await hapticImpact();
       setInputText('');
       setReplyTo(null);
       setMessages((prev) => {
